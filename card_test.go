a7ce15da6e577dbed10bf11140be26f61be85f6c
GvPau <pau.gv@students.salle.url.edu>
2022-10-02 20:41:50 +0200

    Implements New function to create a deck of cards

--- card_test.go
+++ card_test.go
@@ -1,10 +1,21 @@
 package deck
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
 
 func ExampleCard() {
 	fmt.Println(Card{Rank: Ace, Suit: Heart})
 
 	//Output:
 	// Ace of Hearts
+}
+
+func TestNew(t *testing.T) {
+	cards := New()
+
+	if len(cards) != 13*4 {
+		t.Error("Wrong number of cards in a new deck.")
+	}
 }
